cda46ed4fb4e4aafc606824d0447d672e8b4a1d1
Nikita <7jay94@mail.ru>
2021-01-11 23:06:12 +0200

    H_W lesson4-3

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -8,6 +8,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.urise.webapp.storage.AbstractArrayStorage.STORAGE_LIMIT;
+
 public abstract class AbstractArrayStorageTest {
     private Storage storage;
 
@@ -15,7 +17,6 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
     private static final String UUID_4 = "uuid4";
-    private int STORAGE_LIMIT = 10_000;
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -39,6 +40,7 @@ public abstract class AbstractArrayStorageTest {
     public void save() throws Exception {
         storage.save(new Resume(UUID_4));
         Assert.assertEquals(4, storage.size());
+        Assert.assertEquals(storage.get(UUID_2), new Resume(UUID_2));
     }
 
     @Test(expected = ExistStorageException.class)
@@ -48,22 +50,27 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = StorageException.class)
     public void saveOverflow() throws Exception {
-        for (int i = 3; i < STORAGE_LIMIT; i++) {
-            storage.save(new Resume());
+        try {
+            for (int i = 3; i < STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail("\n" +
+                    "Storage was full ahead of time");
         }
         storage.save(new Resume());
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() throws Exception {
         storage.delete(UUID_2);
         Assert.assertEquals(2, storage.size());
+        Assert.assertTrue(storage.get(UUID_2) == new Resume(UUID_2));
     }
 
     @Test(expected = NotExistStorageException.class)
     public void deleteNoExist() throws Exception {
-        storage.delete(UUID_2);
-        storage.delete(UUID_2);
+        storage.delete(UUID_4);
     }
 
     @Test
@@ -75,7 +82,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void updateNoExist() throws Exception {
-        storage.get("UUID_5");
+        storage.get("UUID_4");
     }
 
     @Test
@@ -86,7 +93,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void getNoExist() throws Exception {
-        storage.get("dummy");
+        storage.get("UUID_4");
     }
 
     @Test
